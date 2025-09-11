a6f4e66aa97dd7892ae38dac860d910cc42731d4
Tu Nombre <tu_correo@loquesea.com>
2025-09-10 20:00:33 -0500

    Update LoanValidationApplication.java

--- loan-validation/src/main/java/com/techgirls/loanvalidation/LoanValidationApplication.java
+++ loan-validation/src/main/java/com/techgirls/loanvalidation/LoanValidationApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
-* Loan Validation Microservice entry point.
-*/
+ * Loan Validation Microservice entry point.
+ */
 @SpringBootApplication
 public class LoanValidationApplication {
     public static void main(String[] args) {
